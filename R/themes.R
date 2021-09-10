c3125fe7f94ca39b8b69d65d2e18511eaf491878
Ahmad <ahmad.ehyaei@gmail.com>
2021-09-10 18:07:26 +0200

    add some features to theme

--- R/themes.R
+++ R/themes.R
@@ -26,24 +26,29 @@ theme_scientific <- function(base_size = 12){
 
   theme_bw(base_size = base_size, base_family = "Univers") +
     theme(
-      panel.border = element_rect(color = NA),
+      panel.border = element_blank(),
+      panel.background = element_blank(),
       plot.background = element_blank(),
-      panel.grid.major = element_line(color = "gray70", size = .25, linetype = 2),
-      panel.grid.minor = element_line(color = "gray80", size = .15, linetype = 2),
+      legend.box.background = element_blank(),
+      legend.background = element_rect(fill = "transparent"),
+      panel.grid.major = element_line(color = "gray70", size = .2, linetype = "dotted"),
+      panel.grid.minor = element_line(color = "gray80", size = .1, linetype = "dotted"),
       axis.line.x = element_line(color = "gray40",size = .4),
       axis.line.y = element_line(color = "gray40",size = .4),
       axis.ticks = element_line(color = "gray20"),
       legend.key = element_rect(fill = NA, color = NA),
       legend.text = element_text(size = rel(.8), color = "black"),
       plot.title = element_text(color = "gray20", size = rel(1.2),face = "bold", hjust = .5),
-      axis.text.x = element_text(size = rel(.9), color = "gray20"),
-      axis.text.y = element_text(size = rel(.9), color = "gray20"),
-      axis.title.x = element_text(size = rel(1), color = "gray20", vjust = 0),
-      axis.title.y = element_text(size = rel(1), color="gray20", vjust = 1),
+      axis.text.x = element_text(size = rel(.8), color = "gray20"),
+      axis.text.y = element_text(size = rel(.8), color = "gray20"),
+      axis.title.x = element_text(size = rel(0.9), color = "gray20", vjust = 0),
+      axis.title.y = element_text(size = rel(0.9), color="gray20", vjust = 1),
       plot.margin = margin(25, 25, 25, 25),
-      legend.background = element_blank(),
       plot.caption = element_text(size = rel(0.9), face = "italic")
+
     )
 
 }
+
+
 
