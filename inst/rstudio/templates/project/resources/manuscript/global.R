13966192eeb47684b635c94e5f85030984f8b725
Ahmad <ahmad.ehyaei@gmail.com>
2021-09-26 22:25:43 +0200

    edit bookdown skeleton

--- inst/rstudio/templates/project/resources/manuscript/global.R
+++ inst/rstudio/templates/project/resources/manuscript/global.R
@@ -1,3 +1,23 @@
+############################################################
+#                                                          #
+#                       Set Options                        #
+#                                                          #
+############################################################
+
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  dev.args = list(bg = 'transparent'),
+  warning = F,
+  message = F,
+  error = F,
+  fig.align = "center",
+  fig.pos ="H",
+  comment = "",
+  highlight = TRUE,
+  screenshot.opts <- list(delay = 1),
+  fig.path = "figures/"
+)
+
 options(scipen = 999)
 options(knggitr.kable.NA = '')
 
@@ -7,9 +27,16 @@ options(knggitr.kable.NA = '')
 #                                                          #
 ##%######################################################%##
 
+if (!require(hexbin)) install.packages('hexbin')
+if (!require(gt)) install.packages('gt')
+if (!require(kableExtra)) install.packages('kableExtra')
+
 library(MPIThemes)
 library(ggplot2)
-
+library(dplyr)
+library(grid)
+library(gt)
+library(kableExtra)
 
 ############################################################
 #                                                          #
