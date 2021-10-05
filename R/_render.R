1013b6b0e60d4c78b2f59c7a10b10c74c720d490
Ahmad <ahmad.ehyaei@gmail.com>
2021-10-05 15:24:37 +0200

    add pdf to png

--- R/_render.R
+++ R/_render.R
@@ -14,4 +14,13 @@ for(i in 1:length(examples_name)){
   print(i)
 }
 
+## Convert Interior page to PNG
+examples_name = "interior_feature_pdf_report"
+examples_path = paste0("examples/",examples_name,"/",examples_name,".pdf")
+pages <- magick::image_read_pdf(examples_path,density = 150)
+for(i in 2:3){
+  image_write(pages[i], path = paste0("man/figures/",examples_name,"_",i,".png"), format = "png")
+  image_write(pages[i], path = paste0("examples/",examples_name,"/",examples_name,"_",i,".png"), format = "png")
+
+}
 
