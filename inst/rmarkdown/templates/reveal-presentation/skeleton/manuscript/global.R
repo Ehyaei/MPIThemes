06afe9a17620db159f1b78d700ae9400e21de4c0
Ahmad <ahmad.ehyaei@gmail.com>
2021-10-09 16:46:53 +0200

    add revealjs template

---
+++ inst/rmarkdown/templates/reveal-presentation/skeleton/manuscript/global.R
@@ -0,0 +1,46 @@
+############################################################
+#                                                          #
+#                       Set Options                        #
+#                                                          #
+############################################################
+
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  dev.args = list(bg = 'transparent'),
+  warning = FALSE,
+  message = FALSE,
+  error = FALSE,
+  echo = FALSE,
+  dpi = 300,
+  out.width = "100%",
+  fig.align = "center",
+  fig.pos ="H",
+  comment = "",
+  highlight = TRUE,
+  screenshot.opts <- list(delay = 1),
+  fig.path = "figures/"
+)
+
+options(scipen = 999)
+options(knggitr.kable.NA = '')
+
+##%######################################################%##
+#                                                          #
+####                    load library                    ####
+#                                                          #
+##%######################################################%##
+
+library(MPIThemes)
+library(ggplot2)
+library(dplyr)
+library(grid)
+library(gt)
+library(kableExtra)
+
+############################################################
+#                                                          #
+#                   Set Color and Theme                    #
+#                                                          #
+############################################################
+
+set_color_theme(12)
